cef137a0cce4895d6bc065d3625bd0d1850c7fe3
Chris Gascoigne <cgascoig@cisco.com>
2021-11-26 10:35:36 +1100

    use env vars for secrets

--- scripts/notify.py
+++ scripts/notify.py
@@ -1,10 +1,14 @@
 from webexteamssdk import WebexTeamsAPI
-import creds
+import os
+
+WEBEX_TEAMS_TOKEN = os.getenv("WEBEX_TEAMS_TOKEN")
+DESTINATIONS = os.getenv("DESTINATIONS").split(",")
+
 
 def send_notification(msg):
-    api = WebexTeamsAPI(access_token=creds.WEBEX_TEAMS_TOKEN)
+    api = WebexTeamsAPI(access_token=WEBEX_TEAMS_TOKEN)
 
-    to_emails = creds.DESTINATIONS
+    to_emails = DESTINATIONS
 
     for email in to_emails:
         api.messages.create(toPersonEmail=email, markdown=msg)
