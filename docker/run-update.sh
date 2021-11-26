46cf4ccc9ba4fc71926b4c1a360c04ca38dcf4c9
Chris Gascoigne <cgascoig@cisco.com>
2021-11-26 15:38:37 +1100

    add docker image

---
+++ docker/run-update.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+git config --global user.email "cgascoig@cisco.com"
+git config --global user.name "Chris Gascoigne"
+
+BRANCH="${BRANCH:-main}"
+
+git clone "https://pat:${GITHUB_PAT}@github.com/cgascoig/intersight-go-sdk.git"
+cd intersight-go-sdk/scripts
+git checkout "${BRANCH}"
+pipenv sync
+pipenv run ./update.py
